6cec3104ad327d2235d98e9ffb00f7cc5a2f07d9
andreituta <tuta.andrei96@gmail.com>
2022-01-03 11:42:43 +0000

    Add env variables for deployment

--- telegram.py
+++ telegram.py
@@ -1,8 +1,14 @@
 import telebot
+import requests
+import logging
+import os
 
 from datetime import datetime
-import requests
 from pathlib import Path
+
+# logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class Dictionary():
     def __init__(self, lang):
@@ -14,6 +20,7 @@ class Dictionary():
         words = {}
         try:
             with open(self.dict_loc) as fp:
+                logging.info(f'Processing dictionary at {self.dict_loc}')
                 while True:
                     line = fp.readline()        
                     if not line:
@@ -21,12 +28,12 @@ class Dictionary():
                     content = line.split(":")
                     word = content[0]
                     translation = content[1].replace('\n', '')
-                    print(f"{word}: {translation}")
+                    logging.info(f"{word}: {translation}")
                     words[word] = translation
                     words["updated"] = False
                 fp.close()
         except FileNotFoundError:
-            print(f'Dictionary for language {self.language} doesn\'t exist. It will be initialised as empty')
+            logging.info(f'Dictionary for language {self.language} doesn\'t exist. It will be initialised as empty')
             Path(self.dict_loc).touch(exist_ok=True)
         return words
 
@@ -39,13 +46,13 @@ class Dictionary():
                     file1.write("\n")
             file1.close()
         else:
-            print("Not updating word file.")
+            logging.info("Not updating word file.")
 
     def get_word(self, word):
         try:
             return self.words[word]
         except KeyError:
-            print(f"Dictionary doesn't contain word {word}")
+            logging.info(f"Dictionary doesn't contain word {word}")
             return None
 
     def add_word(self, word, translation):
@@ -58,14 +65,15 @@ class Translator():
         self.dict = dict
 
     def translate_message(self, message, lang):
-        translated_message = []
-        for word in message.split(' '):
-            translation = self.translate_word(word, lang)
-            if translation is None:
-                raise Exception("Translation language not found in dictionary")
-            print(f'Processing translation for {translation}')
-            translated_message.append(translation)
-        return ' '.join(translated_message)
+        if message is not None:
+            translated_message = []
+            for word in message.split(' '):
+                translation = self.translate_word(word, lang)
+                if translation is None:
+                    raise Exception("Translation language not found in dictionary")
+                logging.info(f'Processing translation for {translation}')
+                translated_message.append(translation)
+            return ' '.join(translated_message)
 
     def translate_word(self, message_word, lang):
         if (self.dict.language == lang):
@@ -75,33 +83,71 @@ class Translator():
             else: 
                 new_word = message_word
                 translation = self.translate_deepl(message_word)
-                self.dict.add_word(new_word, translation)
-                print(f"Adding a new word to dictionary")
-            self.dict.save_words()
-            return translation
+                if translation is not None:
+                    self.dict.add_word(new_word, translation)
+                    logging.info(f"Adding a new word to dictionary")
+                    self.dict.save_words()
+                    return translation
+                else:
+                    return None
         else:
-            print("Language not found.")
+            logging.info("Language not found.")
             return None
 
     def translate_deepl(self, word):
-        url = f"https://api-free.deepl.com/v2/translate?auth_key={self.key}&text={word}&target_lang=EN"
+        url = f"https://api-free.deepl.com/v2/translate?auth_key={self.key}&text={word}&source_lang={self.dict.language}&target_lang=EN"
         response = requests.request("GET", url, headers={}, data={})
         if(response.status_code == 200):
             text = response.json()
             translations = text['translations']
             translation = translations[0]['text']
-            print(f"Processed translation for {word} using DeepL: {translation}")
+            logging.info(f"Processed translation for {word} using DeepL: {translation}")
             return translation
         else:
-            return "Error translating"
+            logging.info(f"Error translating {word}")
+            return None
 
-d = Dictionary('ru')
-t = Translator(d, input("Enter API key: "))
-# bot = telebot.TeleBot(telegram_key, threaded=False)
+class TranslatorBot():
+    def __init__(self, api_key) -> None:
+        self.bot = telebot.TeleBot(api_key, threaded=False)
+        logging.info('Initialised telebot')
 
-# @bot.message_handler(commands=['identify'])
-# def identify(message):
-#   cid = message.chat.id
+    # method used to parse arguments passed after the command
+    def extract_arg(self, arg):
+        logging.info(f'processing args: {arg}')
+        return arg.split()[1:]
 
-# bot.polling(none_stop=True, interval=3, timeout=10)
+tBot = TranslatorBot(os.environ["TELEGRAM_KEY"])
+bot = tBot.bot
+chat_translators = {}
+
+@bot.message_handler(commands=['help', 'start', 'info'])
+def get_help(message):
+    help_info =  f"""/help /start /info - displays the menu \n /identify [iso lang code]- adds the current chat into a list of translators by creating a translator from [iso lang code] to EN \n /translate [message] - returns the translation of the message to english"""
+   
+    bot.reply_to(message, help_info)
+
+@bot.message_handler(commands=['identify'])
+def identify(message):
+    cid = message.chat.id
+    args = tBot.extract_arg(message.text)
+    if(len(args) == 1):
+        arg = str(args[0])
+        t = Translator(Dictionary(arg), os.environ["DEEPL_KEY"])
+        chat_translators[cid] = t
+        bot.reply_to(message, f"Created translator for {cid} and language {arg}")
+    else:
+        bot.reply_to(message, "Please pass a second param to initialise the translator")
+
+@bot.message_handler(commands=['translate'])
+def translate_message(message):
+    cid = message.chat.id
+    try:
+        t = chat_translators[cid]
+        message_text = message.text.replace('/translate','')
+        bot.reply_to(message, t.translate_message(message_text, t.dict.language))
+    except KeyError:
+        pass
+
+bot.polling(none_stop=True, interval=3, timeout=10)
 
